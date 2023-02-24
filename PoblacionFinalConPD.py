e204a343587b771b02435de3cdfd5d1bf1b02184
daniel <danielbuitragosantana@gmail.com>
2023-02-23 23:22:23 -0500

    se agrega metodo exponecial se modifica censo de ejemplo 3

--- PoblacionFinalConPD.py
+++ PoblacionFinalConPD.py
@@ -1,10 +1,13 @@
-#importa la libreria de pandas
+#importa las librerias de pandas y numpy
 import pandas as pd
+import numpy as np
 
 #leer el archivo .csv y guardarlo como un dataframe en una variable
-dfPob= pd.read_csv('censo.csv',sep=';')
+dfPob= pd.read_csv('censo3.csv',sep=';')
 
 #generar las funciones 
+
+#metodo aritmetico
 def pfArtimetico(df, Tf) :
     k=0
     for i in range(df.index.size - 1):
@@ -12,13 +15,12 @@ def pfArtimetico(df, Tf) :
             fPob=df.iloc[i+1]
             k+=(((fPob.iloc[1])-(iPob.iloc[1]))/((fPob.iloc[0])-(iPob.iloc[0])))
     k=(k/(df.index.size -1))
-    fPob=df.iloc[-1]
+    fnPob=df.iloc[-1]
+    pf = round(fnPob.iloc[1] + (k*(Tf-fnPob.iloc[0])))
 
-    puf = round(fPob.iloc[1] + (k*(Tf-fPob.iloc[0])))
+    return pf
 
-    return puf
-
-
+#metodo geometrico
 def pfGometrico(df, Tf):
     r=0
     for i in range(df.index.size - 1):
@@ -26,10 +28,24 @@ def pfGometrico(df, Tf):
         fPob=df.iloc[i+1]
         r+=(((fPob.iloc[1]/iPob.iloc[1])**(1/(fPob.iloc[0]-iPob.iloc[0])))-1)
     r= r/(df.index.size -1)
-    fPob=df.iloc[-1]
-    puf=round(fPob.iloc[1]*((1+r)**(Tf-fPob.iloc[0])))
-    return puf
+    fnPob=df.iloc[-1]
+    pf=round(fnPob.iloc[1]*((1+r)**(Tf-fnPob.iloc[0])))
+
+    return pf
+
+#metodo exponencial
+def pfExponencial(df, Tf):
+    k=0
+    for i in range(df.index.size -1):
+       iPob=df.iloc[i]
+       fPob=df.iloc[i+1]
+       k+=(np.log(fPob.iloc[1])-np.log(iPob.iloc[1]))/(fPob.iloc[0]-iPob.iloc[0])
+    k=(k/(df.index.size -1))
+    inPob=df.iloc[0]
+    pf=round(inPob.iloc[1]*np.exp(k*(Tf-inPob.iloc[0])))
+    return pf
 
 
-print(pfArtimetico(dfPob,2034))
-print(pfGometrico(dfPob,2034)) 
+print(pfArtimetico(dfPob,2022))
+print(pfGometrico(dfPob,2022))
+print(pfExponencial(dfPob,2022)) 
--- pfMetodos.py
+++ pfMetodos.py
@@ -1,26 +1,35 @@
 import pandas as pd
+import numpy as np
 
 #artimetico
 
 def pfArtimetico(Puc, Pci, Tuc, Tci, Tf) :
 
-    puf = Puc + (((Puc-Pci)/(Tuc-Tci)*(Tf-Tuc)))
+    pf = Puc + (((Puc-Pci)/(Tuc-Tci)*(Tf-Tuc)))
 
     
 
-    return puf
+    return pf
 
 
 #geometrico
 def pfGeometrico(Puc, Pci, Tuc, Tci, Tf) :
 
     r= ((Puc/Pci)**(1/(Tuc-Tci)))-1
-    puf = Puc*(1+r)**((Tf-Tuc))
+    pf = Puc*(1+r)**((Tf-Tuc))
 
     
 
-    return puf
+    return pf
 
-print(pfArtimetico(4000,200,2005,1950,2039))
-print(pfGeometrico(4000,200,2005,1950,2030))
+#exponecial
+def pfExponecial(Puc, Pca, Tuc, Tca, Tf,Pci, Tci):
+    k=(np.log(Puc)-np.log(Pca))/(Tuc-Tca)
+    pf= Pci * np.exp(k*(Tf-Tci))
+    return pf
+
+#print(pfArtimetico(4000,200,2005,1950,2039))
+print(pfArtimetico(2806,1067,2005,1938,2022))
+print(pfGeometrico(2806,1067,2005,1938,2022))
+print(pfExponecial(2806,2440,2005,1993,2022,1067,1938))
 
