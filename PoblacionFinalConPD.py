721591e7d680239ddbd92af342eae359ecca62df
daniel <danielbuitragosantana@gmail.com>
2023-02-23 21:42:32 -0500

    first commit

---
+++ PoblacionFinalConPD.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+dfPob= pd.read_csv('censo.csv',sep=';')
+def pfArtimetico(df, Tf) :
+    k=0
+    for i in range(df.index.size - 1):
+            iPob= df.iloc[i]
+            fPob=df.iloc[i+1]
+            k+=(((fPob.iloc[1])-(iPob.iloc[1]))/((fPob.iloc[0])-(iPob.iloc[0])))
+    k=(k/(df.index.size -1))
+    fPob=df.iloc[-1]
+
+    puf = round(fPob.iloc[1] + (k*(Tf-fPob.iloc[0])))
+
+    return puf
+
+
+def pfGometrico(df, Tf):
+    r=0
+    for i in range(df.index.size - 1):
+        iPob=df.iloc[i]
+        fPob=df.iloc[i+1]
+        r+=(((fPob.iloc[1]/iPob.iloc[1])**(1/(fPob.iloc[0]-iPob.iloc[0])))-1)
+    r= r/(df.index.size -1)
+    fPob=df.iloc[-1]
+    puf=round(fPob.iloc[1]*((1+r)**(Tf-fPob.iloc[0])))
+    return puf
+
+
+print(pfArtimetico(dfPob,2034))
+print(pfGometrico(dfPob,2034)) 
---
+++ pfMetodos.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+#artimetico
+
+def pfArtimetico(Puc, Pci, Tuc, Tci, Tf) :
+
+    puf = Puc + (((Puc-Pci)/(Tuc-Tci)*(Tf-Tuc)))
+
+    
+
+    return puf
+
+
+def pfGeometrico(Puc, Pci, Tuc, Tci, Tf) :
+
+    r= ((Puc/Pci)**(1/(Tuc-Tci)))-1
+    puf = Puc*(1+r)**((Tf-Tuc))
+
+    
+
+    return puf
+
+print(pfArtimetico(4000,200,2005,1950,2039))
+print(pfGeometrico(4000,200,2005,1950,2030))
+
